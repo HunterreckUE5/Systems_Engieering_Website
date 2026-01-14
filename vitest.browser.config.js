f4ff6b8dd8d7ec635e411ff176f75a007d24a128
Albert Rico Martin <ari41732@stud.hs-furtwangen.de>
2026-01-14 15:24:30 +0100

    - Installed packagge vitest & ESlint - Created a couple example unti tests

---
+++ vitest.browser.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vitest/config'
+import { preview } from '@vitest/browser-preview'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    browser: {
+      enabled: true,
+      provider: preview(),
+      instances: [
+        { browser: 'firefox' }
+      ],
+      include: ['./test/*']
+    },
+  },
+})
